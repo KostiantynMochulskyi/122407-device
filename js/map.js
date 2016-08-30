54102e0d3a44c0e5f4d291a74656a48d92fffba6
KostiantynMochulskyi <mochulskiy.constantine@gmail.com>
2016-08-30 15:23:45 +0300

    catalog DEVICE degree

--- js/map.js
+++ js/map.js
@@ -1,20 +1,23 @@
-			var map = document.querySelector(".map a");
-			var popupmap = document.querySelector(".modal-map");
-			var closemap = popupmap.querySelector("close-modal");
-			var overlay = document.querySelector(".modal-overlay");
+				var map = document.querySelector(".map a");
+				var popupmap = document.querySelector(".modal-map");
+				var closemap = popupmap.querySelector(".close-modal");
+				var overlay = document.querySelector(".modal-overlay");
 
-			map.addEventListener("click", function(event){
+				map.addEventListener("click", function(event){
 				event.preventDefault();
 				overlay.classList.add("modal-overlay-show");
 				popupmap.classList.add("modal-map-show");
-			});			
-			overlay.addEventListener("click", function(event){
+				});
+
+				overlay.addEventListener("click", function(event){
 				event.preventDefault();
 				overlay.classList.remove("modal-overlay-show");
 				popup.classList.remove("modal-writeus-show");
 				popupmap.classList.remove("modal-map-show");
-			});
-			closemap.addEventListener("click", function(event){
+				});
+
+				closemap.addEventListener("click", function(event){
 				event.preventDefault();
 				popupmap.classList.remove("modal-map-show");
-			});
+				overlay.classList.remove("modal-overlay-show");
+				});
