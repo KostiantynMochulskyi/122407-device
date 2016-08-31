91398db8deeebcf49c297369080e59bb5fbe9903
KostiantynMochulskyi <mochulskiy.constantine@gmail.com>
2016-08-31 18:58:18 +0300

    project DEVICE

---
+++ js/heightcatalog.js
@@ -0,0 +1,5 @@
+			var block1 = document.querySelector(".catalog-content");
+			var height1 = block1.scrollHeight;
+			var block2 = document.querySelector(".left-content");
+			
+			block2.style.height = height1 +'px';
